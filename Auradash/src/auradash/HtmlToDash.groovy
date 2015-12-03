94f666368d3eaf51fd4576cc1e8cebe9c54143f6
nkruk <nkruk@salesforce.com>
2015-12-03 16:24:58 -0500

    test

--- Auradash/src/auradash/HtmlToDash.groovy
+++ Auradash/src/auradash/HtmlToDash.groovy
@@ -1,8 +1,16 @@
 package auradash
 
 class HtmlToDash {
+	
 	static void main(def args){
 		def mylist= [1,2,"Lars","4"]
 		mylist.each{ println it }
 	}
+	
+	class Component {
+		String namespace
+		String name
+		def attributeList		
+	}
+		
 }
